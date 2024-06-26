937e830c9939abfe48d542e975b4d5a2feee429b
seung3970 <seung3970@gmail.com>
2024-06-26 15:38:28 +0900

    skjsskk

---
+++ aas/src/main/java/join/JoinDTO.java
@@ -0,0 +1,17 @@
+package com.example.join;
+
+public class JoinDTO {
+
+	 private String user_id;
+	 private String pwd;
+	 private String user_name;
+	 private String tel_num;
+	 private String addr1;
+	 private String addr2;
+	 private String email;
+	 private String snum;
+	 private String auth;
+	 private String level_name;
+	 private Integer point;
+	 
+}
