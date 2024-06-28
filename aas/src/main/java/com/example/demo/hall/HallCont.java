1ace40da633b2f6156eb30326a642bd2945c1a9c
lumencloud <sangyuop@gmail.com>
2024-06-28 16:08:01 +0900

    update

---
+++ aas/src/main/java/com/example/demo/hall/HallCont.java
@@ -0,0 +1,32 @@
+package com.example.demo.hall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HallCont {
+	
+	public HallCont() {
+		System.out.println("----------HallCont 객체생성완료");
+	}
+
+    @Autowired
+    private HallDAO hallDao;
+    
+    
+    //결과확인
+    //->http://localhost:9095/list
+    
+    @RequestMapping("/list")
+    public ModelAndView list() {
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("hall/hallList");
+        mav.addObject("list", hallDao.list());
+        return mav;
+    }//list() end
+    
+
+}//end HallCont
